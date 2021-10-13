6be8ff8b2953c9cb240deda14e52119d9bdc923f
Ce Manalang <cecillemanalang@gmail.com>
2021-10-13 18:30:09 +0800

    add RubyOutline and SplashBackground to Home component

--- app/javascript/components/pages/Home.jsx
+++ app/javascript/components/pages/Home.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import SharedLayout from 'components/layout/SharedLayout';
+import RubyOutline from 'components/icons/RubyOutline';
+import SplashBackground from 'components/icons/SplashBackground';
+import Button from 'components/Button';
 
 import 'stylesheets/page';
+import 'stylesheets/home';
 
 const Home = () => (
     <SharedLayout>
-        <div className="purple">Homepage</div>
+        <div className="hero">
+            <div className="hero-left">
+                <RubyOutline className="ruby-outline" />
+
+                <div className="hero-cta">
+                    <h1>WNB.rb</h1>
+                    <p>A virtual community for women and non-binary Rubyists.</p>
+                    <a
+                        href="https://tinyurl.com/join-wnb-rb"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        <Button type="secondary">Join WNB.rb</Button>
+                    </a>
+                </div>
+            </div>
+
+            <div className="hero-right">
+                <SplashBackground className="splash-background" />
+            </div>
+        </div>
     </SharedLayout>
 );
 
