f6d9a46e54760c7e2285963fc52a989fa3073732
Ruta R <rutareiso@protonmail.com>
2021-10-20 12:23:01 -0400

    Refactor code to add gemstone icons

--- app/javascript/components/SponsorCard.jsx
+++ app/javascript/components/SponsorCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Button from './Button';
 import PropTypes from 'prop-types';
 
-const SponsorCard = ({ type, text, amount, icon }) => {
+const SponsorCard = ({ type, text, amount, children }) => {
     return (
         <div
-            className="container max-w-xs mx-3 p-4 mt-2 border-gray-200 border rounded-md shadow-lg flex flex-col justify-between"
-            style={{ height: '26rem' }}
+            className="container max-w-xs mx-3 p-4 my-3 border-gray-200 border rounded-md shadow-lg flex flex-col justify-between"
+            style={{ minHeight: '26rem' }}
         >
             <div>
-                {icon('mx-auto p-2')}
+                {children}
                 <h3 className="text-2xl font-bold mx-2 my-3">{type} Sponsor</h3>
                 <p className="m-2">{text}</p>
             </div>
@@ -31,7 +31,7 @@ SponsorCard.propTypes = {
     type: PropTypes.string,
     text: PropTypes.string,
     amount: PropTypes.string,
-    icon: PropTypes.func,
+    children: PropTypes.object,
 };
 
 export default SponsorCard;
