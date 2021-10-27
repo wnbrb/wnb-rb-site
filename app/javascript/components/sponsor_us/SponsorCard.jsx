76d7d98d62212271674285c394b47e779a9ef43d
Ruta Reisoglu <rutareiso@protonmail.com>
2021-10-27 17:11:41 -0400

    Add classes to components and sponsor-us stylesheet

--- app/javascript/components/sponsor_us/SponsorCard.jsx
+++ app/javascript/components/sponsor_us/SponsorCard.jsx
@@ -4,10 +4,7 @@ import PropTypes from 'prop-types';
 
 const SponsorCard = ({ type, text, amount, children }) => {
     return (
-        <div
-            className="container max-w-xs mx-3 p-4 my-3 border-gray-200 border rounded-md shadow-lg flex flex-col justify-between"
-            style={{ minHeight: '26rem', backgroundColor: '#FFFFFF' }}
-        >
+        <div className="sponsor-card">
             <div>
                 {children}
                 <h3 className="text-2xl font-bold mx-2 my-3">{type} Sponsor</h3>
--- app/javascript/components/sponsor_us/SponsorUsInfoCard.jsx
+++ app/javascript/components/sponsor_us/SponsorUsInfoCard.jsx
@@ -8,7 +8,7 @@ const SponsorUsInfoCard = ({ title, section, children }) => {
                 {children}
                 <h3 className="text-2xl font-bold mx-2 my-2">{title}</h3>
             </div>
-            <div className="ml-10 text-base leading-7">{section}</div>
+            <div className="ml-5 sm:ml-10 text-base leading-7">{section}</div>
         </div>
     );
 };
