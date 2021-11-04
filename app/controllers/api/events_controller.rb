89f8eead84ea0fbc44acf2746b8db9fd5846596c
Deanna <deanna.marbeck@gmail.com>
2021-11-04 18:23:09 +0000

    Get events for a past month

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -6,5 +6,15 @@ module Api
       events = Event.where('date < ?', DateTime.current).order(date: :desc)
       render status: 200, json:  { data: events.as_json }
     end
+
+    def past_by_month
+      event_date = DateTime.new(params[:year].to_i, params[:month].to_i)
+      events = Event.where(date: event_date..event_date.end_of_month)
+      if events.empty?
+        render status: 404, json: {data: 'No events found'}
+      else
+        render status: 200, json: {data: events.as_json}
+      end
+    end
   end
 end
