5f3691413eb07d26677f175d3f9940c924e88822
Ariel Herman <ariel.herman@glossier.com>
2021-11-29 14:56:28 -0500

    Fetch meetup data from API

--- app/javascript/components/pages/Meetups.jsx
+++ app/javascript/components/pages/Meetups.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SharedLayout from 'components/layout/SharedLayout';
 import PageTitle from 'components/PageTitle';
 import 'stylesheets/page';
 import 'stylesheets/home';
 
 const Meetups = () => {
+    const [meetups, setMeetups] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch('/api/events/past');
+            const data = await result.json();
+            setMeetups(data.data);
+        };
+
+        fetchData();
+    }, []);
+    console.log(meetups);
+
     return (
         <SharedLayout>
             <PageTitle text="Past Meetups" />
+            <ul className="container mx-auto">
+                {meetups && meetups.length > 0
+                    ? meetups.map((meetup) => (
+                          <li key={meetup.id}>
+                              <h3>{meetup.title}</h3>
+                          </li>
+                      ))
+                    : null}
+            </ul>
         </SharedLayout>
     );
 };
