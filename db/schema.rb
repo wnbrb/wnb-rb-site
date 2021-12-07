baf2f7bb2eb2c9410b961561fe2c5bc3b72e3a92
ty18881 <ty18881@gmail.com>
2021-12-06 21:07:31 -0500

    remove admin, admin helper files and password spec

--- db/schema.rb
+++ db/schema.rb
@@ -47,16 +47,19 @@ ActiveRecord::Schema.define(version: 2021_11_11_012922) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email_address"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
