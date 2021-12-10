91f5dda295e81868f6016f86364dba43a4f0c230
Emily Giurleo <e.m.giurleo@gmail.com>
2021-12-10 15:44:24 -0500

    Add role column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  ADMIN = 'admin'
+  ROLES = [ADMIN].freeze
 
   ## TO-DO - add custom validation for email address ???
   validates :name, presence: true
   validates :email, uniqueness: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:email]
+  validates :role, inclusion: { in: ROLES }, allow_nil: true
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         authentication_keys: [:email]
+
+  def admin?
+    role == ADMIN
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_012922) do
+ActiveRecord::Schema.define(version: 2021_12_10_204019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2021_11_11_012922) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "jobs", force: :cascade do |t|
+    t.string "company"
+    t.string "title"
+    t.text "description"
+    t.string "link"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "speakers", force: :cascade do |t|
     t.string "name"
     t.text "bio"
@@ -60,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_012922) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
