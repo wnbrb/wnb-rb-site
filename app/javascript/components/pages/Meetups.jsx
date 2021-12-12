7aabbbe4d0b4cdb16d934d065a2375db01ddfe27
Ariel Herman <ariel.herman@glossier.com>
2021-12-12 00:34:43 -0500

    Add timeline styling

--- app/javascript/components/pages/Meetups.jsx
+++ app/javascript/components/pages/Meetups.jsx
@@ -5,13 +5,12 @@ import { monthNameFromNumber } from '../../utils/time';
 import SharedLayout from 'components/layout/SharedLayout';
 import PageTitle from 'components/PageTitle';
 import 'stylesheets/page';
-import 'stylesheets/home';
 import 'stylesheets/meetup';
 
 const YearSection = ({ children, year }) => (
     <section key={year}>
         <h2 className="mb-8 text-4xl font-bold">{year}</h2>
-        <ul className="card-list flex flex-col">{children}</ul>
+        <ul className="flex flex-col">{children}</ul>
     </section>
 );
 
@@ -21,37 +20,29 @@ YearSection.propTypes = {
 };
 
 const MonthSection = ({ children, month }) => (
-    <section key={month}>
-        <h3 className="py-1 px-2 w-min border-2 border-red-400 rounded uppercase text-md text-red-400">
-            {month}
-        </h3>
-        <ul className="card-list flex flex-col">{children}</ul>
-    </section>
+    <li key={month} className="meetups__month flex flex-col items-start md:flex-row">
+        <div className="w-28">
+            <h3 className="py-1 px-2 mb-4 w-min border-2 border-red-400 rounded uppercase text-md text-red-400 md:ml-auto">
+                {month}
+            </h3>
+        </div>
+        <div className="meetups__card--border hidden w-px self-stretch mx-6 border-l border-gray-200 md:block" />
+        <div className="meetups__card flex flex-col pb-12 w-full md:w-9/12 md:pb-24">
+            <div className="w-full rounded shadow-lg border-t border-gray-100 overflow-hidden">
+                <ul className="p-4 md:p-12">{children}</ul>
+                <div className="bg-gray-200 text-right">
+                    <button className="my-4 mr-6 py-4 px-8 bg-gray-600 rounded text-white text-md md:text-2xl">
+                        View
+                    </button>
+                </div>
+            </div>
+        </div>
+    </li>
 );
 
 MonthSection.propTypes = {
     month: PropTypes.string,
     children: PropTypes.node,
-};
-
-const MeetupDetails = ({ meetup }) => (
-    <>
-        <div className="timeline self-stretch p-2 mx-4 border-l border-gray-400"></div>
-        <div className="rounded shadow-lg border-t border-gray-100 overflow-hidden w-full md:w-9/12 mb-24">
-            <div className="p-12">
-                <h4 className="text-xl font-bold">{meetup.title}</h4>
-            </div>
-            <div className="bg-gray-200 text-right">
-                <button className="my-4 mr-6 py-4 px-8 bg-gray-600 rounded text-white text-2xl">
-                    View
-                </button>
-            </div>
-        </div>
-    </>
-);
-
-MeetupDetails.propTypes = {
-    meetup: PropTypes.object,
 };
 
 const Meetups = () => {
@@ -82,11 +73,10 @@ const Meetups = () => {
                                           >
                                               {meetups.map((meetup) => {
                                                   return (
-                                                      <li
-                                                          key={meetup.id}
-                                                          className="flex flex-col items-start md:flex-row justify-end"
-                                                      >
-                                                          <MeetupDetails meetup={meetup} />
+                                                      <li key={meetup.id}>
+                                                          <h4 className="text-xl font-bold">
+                                                              {meetup.title}
+                                                          </h4>
                                                       </li>
                                                   );
                                               })}
