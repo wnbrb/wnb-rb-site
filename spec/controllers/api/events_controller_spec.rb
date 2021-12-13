b0a8d3a348ee4889ab07310157ab3e8735d9a1db
Stephanie Minn <12875392+stephanieminn@users.noreply.github.com>
2021-12-13 11:35:05 -0600

    Update event controller spec to reflect new data format

--- spec/controllers/api/events_controller_spec.rb
+++ spec/controllers/api/events_controller_spec.rb
@@ -6,31 +6,39 @@ RSpec.describe Api::EventsController, type: :controller do #rubocop:disable Metr
   before do
     Meetup.destroy_all
   end
-  
+
   describe 'GET #past' do
     before do
-      Meetup.create(title: 'Fake title', location: 'virtual', date: DateTime.now - 1.day)
-      Meetup.create(title: 'Fake title 2', location: 'virtual', date: DateTime.now - 2.weeks)
-      Panel.create(title: 'Fake title 3', location: 'Denver, Colorado', date: DateTime.now + 1.week)
+      Meetup.create(title: 'July Meetup', location: 'virtual', date: DateTime.new(2021, 7, 31, 16).utc)
+      Meetup.create(title: 'August Meetup', location: 'virtual', date: DateTime.new(2021, 8, 31, 16).utc)
+      Panel.create(title: 'Future Panel', location: 'Denver, Colorado', date: DateTime.now + 1.week)
     end
 
-    it 'correctly renders past events' do
+    it 'correctly gets past events' do
       get :past
       expect(response).to have_http_status(200)
 
       body = JSON.parse(response.body)
-
-      expect(body['data'].count).to eq(2)
-      expect(body['data'].map { |item| item['title'] }).to eq(['Fake title', 'Fake title 2'])
+      expect(body['data']['2021'].count).to eq(2)
     end
 
+    it 'renders events in the correct format' do
+      get :past
+      body = JSON.parse(response.body)
+
+      july_meetup = body['data']['2021']['7'].first
+      expect(july_meetup['title']).to eq('July Meetup')
+
+      august_meetup = body['data']['2021']['8'].first
+      expect(august_meetup['title']).to eq('August Meetup')
+    end
   end
 
   describe 'GET #past_by_month' do
     before do
       Meetup.create(title: 'August event', location: 'virtual', date: DateTime.new(2021, 8, 1))
     end
-    
+
     it 'returns one event for a given month' do
       get :past_by_month, params: {year: '2021', month: '8'}
       expect(response).to have_http_status(200)
