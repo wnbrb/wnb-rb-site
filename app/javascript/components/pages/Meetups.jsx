081159f7bd72d8483aeac5482392299f5cb628c9
Stephanie Minn <12875392+stephanieminn@users.noreply.github.com>
2021-12-15 11:35:29 -0600

    Update styling to separate individual meetup cards within month section

--- app/javascript/components/pages/Meetups.jsx
+++ app/javascript/components/pages/Meetups.jsx
@@ -21,22 +21,13 @@ YearSection.propTypes = {
 
 const MonthSection = ({ children, month }) => (
     <li key={month} className="meetups__month flex flex-col items-start md:flex-row">
-        <div className="w-28">
+        <div className="meetups__month--name w-28">
             <h3 className="py-1 px-2 mb-4 w-min border-2 border-red-400 rounded uppercase text-md text-red-400 md:ml-auto">
                 {month}
             </h3>
         </div>
         <div className="meetups__card--border hidden w-px self-stretch mx-6 border-l border-gray-200 md:block" />
-        <div className="meetups__card flex flex-col pb-12 w-full md:w-9/12 md:pb-24">
-            <div className="w-full rounded shadow-lg border-t border-gray-100 overflow-hidden">
-                <ul className="p-4 flex flex-col gap-y-10 md:p-12 md:gap-y-14">{children}</ul>
-                <div className="bg-gray-200 text-right">
-                    <button className="my-4 mr-6 py-4 px-8 bg-gray-600 rounded text-white text-lg md:text-2xl">
-                        View
-                    </button>
-                </div>
-            </div>
-        </div>
+        <ul className="p-4 flex flex-col gap-y-10 md:pt-2 md:p-12 md:gap-y-14">{children}</ul>
     </li>
 );
 
@@ -45,28 +36,34 @@ MonthSection.propTypes = {
     children: PropTypes.node,
 };
 
-const Meetup = ({ speakers, title = '', description = '' }) => (
+const Meetup = ({ speakers, title = '' }) => (
     <li>
-        <h4 className="mb-4 text-xl font-bold text-gray md:text-2xl">{title}</h4>
-        {speakers.length > 0
-            ? speakers.map(({ id, name, tagline }) => (
-                  <div key={id} className="flex align-start mb-4 text-lg">
-                      <div className="mr-4 w-16 h-16 flex-none rounded-full bg-gray-400"></div>
-                      <div>
-                          <p className="font-bold text-gray md:text-lg">{name}</p>
-                          <p className="text-sm text-gray md:text-lg">{tagline}</p>
-                      </div>
-                  </div>
-              ))
-            : null}
-        <p className="text-lg">{description}</p>
+        <div className="meetups__card flex flex-col pb-12 w-[600px]">
+            <div className="w-full rounded shadow-lg border-t p-10 border-gray-100 overflow-hidden">
+                <h4 className="mb-4 text-xl font-bold text-gray md:text-2xl">{title}</h4>
+                {speakers.length > 0
+                    ? speakers.map(({ id, name, tagline }) => (
+                          <div key={id} className="flex align-start mb-4 text-lg">
+                              <div>
+                                  <p className="font-bold text-gray md:text-lg">{name}</p>
+                                  <p className="text-sm text-gray md:text-lg">{tagline}</p>
+                              </div>
+                          </div>
+                      ))
+                    : null}
+            </div>
+            <div className="bg-gray-200 text-right">
+                <button className="my-4 mr-6 py-4 px-8 bg-gray-600 rounded text-white text-lg md:text-xl">
+                    View
+                </button>
+            </div>
+        </div>
     </li>
 );
 
 Meetup.propTypes = {
     speakers: PropTypes.array,
     title: PropTypes.string,
-    description: PropTypes.string,
 };
 
 const Meetups = () => {
@@ -84,7 +81,7 @@ const Meetups = () => {
     return (
         <SharedLayout>
             <PageTitle text="Past Meetups" />
-            <div className="container mx-auto max-w-6xl px-6 md:px-44 lg:mx-0">
+            <div className="container flex mx-auto px-6 md:px-44 lg:mx-0">
                 {meetupsByYear.length > 0
                     ? meetupsByYear.map(([year, meetupsByMonth]) => {
                           return (
@@ -95,18 +92,15 @@ const Meetups = () => {
                                               key={month_number}
                                               month={monthNameFromNumber(month_number)}
                                           >
-                                              {meetups.map(
-                                                  ({ id, speakers, title, description }) => {
-                                                      return (
-                                                          <Meetup
-                                                              key={id}
-                                                              speakers={speakers}
-                                                              title={title}
-                                                              description={description}
-                                                          />
-                                                      );
-                                                  },
-                                              )}
+                                              {meetups.map(({ id, speakers, title }) => {
+                                                  return (
+                                                      <Meetup
+                                                          key={id}
+                                                          speakers={speakers}
+                                                          title={title}
+                                                      />
+                                                  );
+                                              })}
                                           </MonthSection>
                                       );
                                   })}
