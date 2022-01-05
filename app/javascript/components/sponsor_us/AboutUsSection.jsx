4701843a82ee1248f979e481d09c6f61595799b7
Emily Giurleo <e.m.giurleo@gmail.com>
2022-01-05 17:26:08 -0500

    Update blurbs to match one-pager

--- app/javascript/components/sponsor_us/AboutUsSection.jsx
+++ app/javascript/components/sponsor_us/AboutUsSection.jsx
@@ -8,15 +8,8 @@ const AboutUsSection = () => {
                 levels from recent bootcamp grads to CTOs.
             </p>
             <p className="m-2 pt-4">
-                Our community-driven initiatives include:
-                <ul className={'list-disc ml-10'}>
-                    <li>A monthly meetup</li>
-                    <li>An interview study group</li>
-                    <li>A conference proposal working group</li>
-                    <li>A technical book club</li>
-                    <li>An active Slack workspace</li>
-                    <li>... and more!</li>
-                </ul>
+                Our community-driven initiatives include a monthly meetup, an interview study group,
+                a conference proposal working group, a book club, and more!
             </p>
         </>
     );
--- app/javascript/components/sponsor_us/WhySponsorUsSection.jsx
+++ app/javascript/components/sponsor_us/WhySponsorUsSection.jsx
@@ -7,10 +7,6 @@ const WhySponsorUsSection = () => {
                 <li>Gain brand exposure to a rapidly growing community of 300+ Rubyists</li>
                 <li>Recruit women and non-binary Ruby developers via our jobs board</li>
                 <li>Contribute to making the Ruby community more welcoming and diverse</li>
-                <li>
-                    Demonstrate your company’s commitment to supporting women and non-binary people
-                    in tech
-                </li>
             </ul>
         </>
     );
