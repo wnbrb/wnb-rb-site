05a785a55123ee8643fe249d3802a2bbe6253ac8
Emily Giurleo <e.m.giurleo@gmail.com>
2022-01-17 22:07:25 -0500

    Add image_url to jobs table

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Job < ApplicationRecord
-  validates :company, :title, :description, :link, :location, presence: true
+  validates :company, :title, :description, :link, :location, :image_url, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_204019) do
+ActiveRecord::Schema.define(version: 2022_01_18_025538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_204019) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   create_table "speakers", force: :cascade do |t|
