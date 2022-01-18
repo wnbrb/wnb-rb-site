e731972a7bb5b9216890cd89304b4b6d5b1d33f0
Ce Manalang <cecillemanalang@gmail.com>
2022-01-18 17:46:12 +0800

    feat(sponsorship-section): mobile styles

--- app/javascript/components/SponsorsTile.jsx
+++ app/javascript/components/SponsorsTile.jsx
@@ -6,7 +6,7 @@ import 'stylesheets/sponsors-tile';
 const SponsorsTile = ({ sponsors }) => (
     <div className="sponsors-tile">
         <div className="text-center text-lg">Sponsored by</div>
-        <div className="flex flex-col sm:flex-row items-center h-36 pb-8">
+        <div className="flex items-center h-36 pb-8">
             {sponsors.map((sponsor) => (
                 <div key={sponsor.id}>
                     <img src={sponsor.tile_image} alt={sponsor.tile_image_alt} />
