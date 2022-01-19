81f528f246b00453e99cefe1f15b32caa3f72517
Emily Giurleo <e.m.giurleo@gmail.com>
2022-01-19 10:50:15 -0500

    WIP: job board UI

--- app/javascript/components/Button.jsx
+++ app/javascript/components/Button.jsx
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 
 import 'stylesheets/button';
 
-const Button = ({ children, type }) => <div className={`button ${type}`}>{children}</div>;
+const Button = ({ children, type, className }) => (
+    <div className={`button ${className} ${type}`}>{children}</div>
+);
 
 Button.propTypes = {
     children: PropTypes.node,
     type: PropTypes.string,
+    className: PropTypes.string,
 };
 
 export default Button;
--- app/javascript/components/pages/Jobs.jsx
+++ app/javascript/components/pages/Jobs.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import propTypes from 'prop-types';
 import SharedLayout from 'components/layout/SharedLayout';
 import PageTitle from 'components/PageTitle';
+import Button from 'components/Button';
 import { getJobs } from '../../datasources';
 
 import 'stylesheets/page';
@@ -19,38 +20,74 @@ const Jobs = () => {
         fetchData();
     }, []);
 
-    console.log({ jobs });
-
     return (
         <SharedLayout>
             <PageTitle text="Jobs" />
-            {jobs.map((job) => (
-                <Job
-                    key={`${job.title} at ${job.company}`}
-                    company={job.company}
-                    title={job.title}
-                    description={job.description}
-                    imageUrl={job.image_url}
-                    link={job.link}
-                    location={job.location}
-                    createdAt={job.created_at}
-                />
-            ))}
+            <div className="jobs md:px-44 px-6">
+                {jobs.map((job) => (
+                    <Job
+                        key={`${job.title} at ${job.company}`}
+                        company={job.company}
+                        title={job.title}
+                        description={job.description}
+                        imageUrl={job.image_url}
+                        link={job.link}
+                        location={job.location}
+                        createdAt={job.created_at}
+                    />
+                ))}
+            </div>
         </SharedLayout>
     );
 };
 
 const Job = ({ title, description, imageUrl, company, link, location, createdAt }) => {
+    const postedAt = useMemo(() => {
+        const today = new Date();
+        const postedDate = new Date(createdAt);
+        const msInDay = 24 * 60 * 60 * 1000;
+
+        const diff = (+today - +postedDate) / msInDay;
+
+        let postedDescription;
+
+        if (diff <= 1) {
+            postedDescription = 'today';
+        } else if (diff < 30) {
+            postedDescription = `${diff} days ago`;
+        } else {
+            const monthDiff = Math.floor(diff / 30);
+
+            if (monthDiff === 1) {
+                postedDescription = '1 month ago';
+            } else {
+                postedDescription = `${diff} months ago`;
+            }
+        }
+
+        return `Posted ${postedDescription}`;
+    }, [createdAt]);
+
     return (
-        <>
-            <p>{title}</p>
-            <p>{description}</p>
-            <p>{imageUrl}</p>
-            <p>{company}</p>
-            <p>{link}</p>
-            <p>{location}</p>
-            <p>{createdAt}</p>
-        </>
+        <div className="bg-white shadow-lg rounded-lg my-5 p-10 max-w-sm">
+            <div className="flex flex-row">
+                <img className="w-14 h-14 rounded-full mr-6" src={imageUrl} alt="" />
+                <div className="flex flex-col">
+                    <h2 className="font-bold text-lg text-[#4a4a4a]">{title}</h2>
+                    <div className="mt-1">{company}</div>
+                    <div className="font-light">{location}</div>
+                </div>
+            </div>
+            <div className="my-4">{description}</div>
+            <div className="flex flex-row justify-between align-center space-x-5">
+                <Button type="white" className="w-20">
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                        Apply
+                    </a>
+                </Button>
+                <div className="">{postedAt}</div>
+            </div>
+        </div>
     );
 };
 
@@ -63,5 +100,5 @@ Job.propTypes = {
     imageUrl: propTypes.string,
     link: propTypes.string,
     location: propTypes.string,
-    createdAt: propTypes.date,
+    createdAt: propTypes.string,
 };
