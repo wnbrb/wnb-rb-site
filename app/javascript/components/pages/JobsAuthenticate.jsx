d7d2f8fed12217c18d052b0274aa5a34b6e71364
Emily Giurleo <e.m.giurleo@gmail.com>
2022-01-21 16:07:14 -0500

    Create password protected page

---
+++ app/javascript/components/pages/JobsAuthenticate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SharedLayout from 'components/layout/SharedLayout';
+
+import 'stylesheets/page';
+import 'stylesheets/jobs_authenticate';
+
+const JobsAuthenticate = () => {
+    return <SharedLayout></SharedLayout>;
+};
+
+export default JobsAuthenticate;
