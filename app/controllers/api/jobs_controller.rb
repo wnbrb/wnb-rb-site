6b3cc974e1dc6d2c2fea36194688097734aa2ea8
Emily Giurleo <e.m.giurleo@gmail.com>
2022-01-21 23:15:57 -0500

    Implement authentication behavior when fetching jobs

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -5,19 +5,37 @@ module Api
     skip_before_action :verify_authenticity_token, only: [:authenticate]
 
     def index
+      JWT.decode bearer_token, hmac_secret, true, { algorithm: 'HS256' }
+
       jobs = Job.all
       render status: 200, json: { data: jobs.as_json }
+    rescue JWT::ExpiredSignature, JWT::DecodeError
+      render status: 401, json: {}
     end
 
     def authenticate
-      if params[:password] == ENV['JOB_BOARD_PASSWORD']
-        token = JWT.encode({ expires: Time.now + 7.days }, ENV['JWT_HMAC_SECRET'], 'HS256')
+      if params[:password] == job_board_password
+        exp_time = (Time.now + 7.days).to_i
+        token = JWT.encode({ data: '', exp: exp_time }, hmac_secret, 'HS256')
 
-        # cookies[:token] = { value: token, expires: 7.days }
         render status: 201, json: { token: token }
       else
         render status: 401, json: {}
       end
+    end
+
+    private
+
+    def bearer_token
+      request.headers['Authorization'].split.last
+    end
+
+    def hmac_secret
+      ENV['JWT_HMAC_SECRET']
+    end
+
+    def job_board_password
+      ENV['JOB_BOARD_PASSWORD']
     end
   end
 end
--- spec/controllers/api/jobs_controller_spec.rb
+++ spec/controllers/api/jobs_controller_spec.rb
@@ -3,52 +3,82 @@
 require './spec/rails_helper'
 
 RSpec.describe Api::JobsController, type: :controller do
+  before do
+    ENV['JOB_BOARD_PASSWORD'] = 'wnb.rbRULEZ!'
+    ENV['JWT_HMAC_SECRET'] = '1234567'
+  end
+
+  after do
+    ENV['JOB_BOARD_PASSWORD'] = nil
+    ENV['JWT_HMAC_SECRET'] = nil
+  end
+
   describe 'GET #index' do
-    before { 3.times { FactoryBot.create(:job) } }
+    before { request.headers['Authorization'] = "Bearer #{token}" }
 
-    it 'correctly renders past events' do
-      get :index
-      expect(response).to have_http_status(200)
+    context 'when expired bearer token is provided' do
+      let(:token) do
+        JWT.encode({ data: '', exp: (Time.now - 1.day).to_i }, ENV['JWT_HMAC_SECRET'], 'HS256')
+      end
 
-      body = JSON.parse(response.body)
+      it 'returns 401 response' do
+        get :index
+        expect(response).to have_http_status(401)
+      end
+    end
 
-      expect(body['data'].length).to eq(3)
-      expect(body['data'].first).to include(
-        {
-          company: 'WNB.rb',
-          title: 'Organizer',
-          description: 'Organize WNB.rb',
-          link: 'link-to-job.com',
-          location: 'Remote',
-        }.stringify_keys,
-      )
+    context 'when invalid bearer token is provided' do
+      let(:token) do
+        JWT.encode({ data: '', exp: (Time.now + 1.day).to_i }, 'random_secret', 'HS256')
+      end
+
+      it 'returns 401 response' do
+        get :index
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context 'when valid bearer token is provided' do
+      let(:token) do
+        JWT.encode({ data: '', exp: (Time.now + 1.day).to_i }, ENV['JWT_HMAC_SECRET'], 'HS256')
+      end
+
+      before { 3.times { FactoryBot.create(:job) } }
+
+      it 'correctly renders past events' do
+        get :index
+        expect(response).to have_http_status(200)
+
+        body = JSON.parse(response.body)
+
+        expect(body['data'].length).to eq(3)
+        expect(body['data'].first).to include(
+          {
+            company: 'WNB.rb',
+            title: 'Organizer',
+            description: 'Organize WNB.rb',
+            link: 'link-to-job.com',
+            location: 'Remote',
+          }.stringify_keys,
+        )
+      end
     end
   end
 
   describe 'POST #authenticate' do
-    before do
-      ENV['JOB_BOARD_PASSWORD'] = 'wnb.rbRULEZ!'
-      ENV['JWT_HMAC_SECRET'] = '1234567'
-    end
-
-    after do
-      ENV['JOB_BOARD_PASSWORD'] = nil
-      ENV['JWT_HMAC_SECRET'] = nil
-    end
-
     context 'when password is valid' do
-      it 'returns 201 response and sets JWT in cookie' do
+      it 'returns 201 response and returns JWT' do
         post :authenticate, params: { password: 'wnb.rbRULEZ!' }
         expect(response).to have_http_status(201)
-        expect(cookies[:token]).not_to be_nil
+        expect(JSON.parse(response.body)['token']).not_to be_nil
       end
     end
 
     context 'when password is not valid' do
-      it 'returns 401 response and does not set cookies' do
+      it 'returns 401 response and does not return token' do
         post :authenticate, params: { password: 'invalidpassword' }
         expect(response).to have_http_status(401)
-        expect(cookies[:token]).to be_nil
+        expect(JSON.parse(response.body)['token']).to be_nil
       end
     end
   end
