5857978630885d1d9f8a60bdf1ed3142915c445a
Emily Giurleo <e.m.giurleo@gmail.com>
2022-02-15 19:25:00 -0500

    Fix the code of conduct list on the homepage

--- app/javascript/components/home/GiveSupportSection.jsx
+++ app/javascript/components/home/GiveSupportSection.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const GiveSupportSection = () => {
     return (
-        <>
+        <p>
             Whether you’re looking to mentor others or be mentored yourself, WNB.rb is ready for
             you! Post in our #advice channel to get the perspective of someone who’s been in your
             shoes, or jump into the #ruby-questions channel to help someone struggling with a
             technical problem.
-        </>
+        </p>
     );
 };
 export default GiveSupportSection;
--- app/javascript/components/home/JoinOurWelcomingCommunitySection.jsx
+++ app/javascript/components/home/JoinOurWelcomingCommunitySection.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 const JoinOurWelcomingCommunitySection = () => {
     return (
-        <>
+        <p>
             Attend our monthly meetup or start up a conversation in Slack to begin interacting with
             our community of over 350 women and non-binary Rubyists. Here at WNB.rb, we value safety
-            above all else; that is why we have a code of conduct [LINK] to to prevent gatekeeping
-            and other unsavory behavior.
-        </>
+            above all else; that is why we have a{' '}
+            <a href="https://tinyurl.com/wnb-rb-coc" target="_blank" rel="noopener noreferrer">
+                code of conduct
+            </a>{' '}
+            to to prevent gatekeeping and other unsavory behavior.
+        </p>
     );
 };
 
