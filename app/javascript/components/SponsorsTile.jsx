900caa4e0f33dfc184c21ae0ab5f4401e5116ba8
Emily Giurleo <e.m.giurleo@gmail.com>
2022-02-21 19:24:34 -0500

    Update our sponsors and improve layout

--- app/javascript/components/SponsorsTile.jsx
+++ app/javascript/components/SponsorsTile.jsx
@@ -3,26 +3,20 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 
 import Stripe from 'images/sponsors/stripe.svg';
-import Shopify from 'images/sponsors/shopify.svg';
-import Contentful from 'images/sponsors/contentful.svg';
+import Cleo from 'images/sponsors/cleo.svg';
 
 import 'stylesheets/sponsors-tile';
 
 const sponsors = [
     {
-        id: 1,
         tile_image: Stripe,
         tile_image_alt: 'Stripe',
+        link: 'https://stripe.com',
     },
     {
-        id: 2,
-        tile_image: Shopify,
-        tile_image_alt: 'Shopify',
-    },
-    {
-        id: 3,
-        tile_image: Contentful,
-        tile_image_alt: 'Contentful',
+        tile_image: Cleo,
+        tile_image_alt: 'Cleo',
+        link: 'https://web.meetcleo.com',
     },
 ];
 
@@ -32,7 +26,14 @@ const SponsorsTile = () => (
             <div className="sponsors-tile-title">Our Sponsors</div>
             <div className="sponsors-tile-img-container">
                 {sponsors.map((sponsor) => (
-                    <img key={sponsor.id} src={sponsor.tile_image} alt={sponsor.tile_image_alt} />
+                    <a
+                        key={sponsor.tile_image_alt}
+                        href={sponsor.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={sponsor.tile_image} alt={sponsor.tile_image_alt} />
+                    </a>
                 ))}
             </div>
         </Card>
