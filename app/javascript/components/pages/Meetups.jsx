7149b033bf942b5c960518583144ec6ab497104a
Deanna <deanna.marbeck@gmail.com>
2022-04-09 19:19:13 +0100

    Add talk titles to meetups page

--- app/javascript/components/pages/Meetups.jsx
+++ app/javascript/components/pages/Meetups.jsx
@@ -36,44 +36,60 @@ MonthSection.propTypes = {
     children: PropTypes.node,
 };
 
-const Meetup = ({ speakers, title = '' }) => (
-    <li>
-        <div className="meetups__card flex flex-col pb-12 w-[600px]">
-            <div className="w-full rounded shadow-lg border-t p-10 border-gray-100 overflow-hidden">
-                <h4 className="mb-4 text-xl font-bold text-gray md:text-2xl">{title}</h4>
-                {speakers.length > 0
-                    ? speakers.map(({ id, name, tagline, image_url }) => (
-                          <div key={id} className="flex content-center mb-8 text-lg">
-                              <img
-                                  className="object-cover w-14 h-14 mr-4 rounded-full"
-                                  src={image_url}
-                                  alt=""
-                              />
-                              <div>
-                                  <p className="font-bold text-gray md:text-lg">{name}</p>
-                                  <p className="text-sm text-gray md:text-lg">{tagline}</p>
-                              </div>
-                          </div>
-                      ))
-                    : null}
+const Meetup = ({ speakers, title = '', event_speakers }) => {
+    const eventWithSpeaker = event_speakers.map((talk) => {
+        const speaker = speakers.find((speak) => speak.id === talk.speaker_id);
+        return { ...talk, speaker };
+    });
+    return (
+        <li>
+            <div className="meetups__card flex flex-col pb-12 w-[600px]">
+                <div className="w-full rounded shadow-lg border-t p-10 border-gray-100 overflow-hidden">
+                    <h4 className="mb-4 text-xl font-bold text-gray md:text-2xl">{title}</h4>
+                    {eventWithSpeaker.length > 0 &&
+                        eventWithSpeaker.map(({ id, talk_title, speaker }) => (
+                            <div key={id}>
+                                <h5 className="mb-4 text-xl font-bold text-gray md:text-2xl">
+                                    {talk_title}
+                                </h5>
+                                <div className="flex content-center mb-8 text-lg">
+                                    <img
+                                        className="object-cover w-14 h-14 mr-4 rounded-full"
+                                        src={speaker.image_url}
+                                        alt=""
+                                    />
+                                    <div>
+                                        <p className="font-bold text-gray md:text-lg">
+                                            {speaker.name}
+                                        </p>
+                                        <p className="text-sm text-gray md:text-lg">
+                                            {speaker.tagline}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+                <div className="bg-gray-200 text-right">
+                    <button className="my-4 mr-6 py-4 px-8 bg-gray-600 rounded text-white text-lg md:text-xl">
+                        View
+                    </button>
+                </div>
             </div>
-            <div className="bg-gray-200 text-right">
-                <button className="my-4 mr-6 py-4 px-8 bg-gray-600 rounded text-white text-lg md:text-xl">
-                    View
-                </button>
-            </div>
-        </div>
-    </li>
-);
+        </li>
+    );
+};
 
 Meetup.propTypes = {
     speakers: PropTypes.array,
     title: PropTypes.string,
+    event_speakers: PropTypes.array,
 };
 
 const Meetups = () => {
     const [loading, setLoading] = useState(true);
     const [meetupsByYear, setMeetupsByYear] = useState({});
+    console.log('meetups', meetupsByYear);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -99,15 +115,18 @@ const Meetups = () => {
                                       {Object.entries(meetupsByMonth).map(([month, meetups]) => {
                                           return (
                                               <MonthSection key={month} month={month}>
-                                                  {meetups.map(({ id, speakers, title }) => {
-                                                      return (
-                                                          <Meetup
-                                                              key={id}
-                                                              speakers={speakers}
-                                                              title={title}
-                                                          />
-                                                      );
-                                                  })}
+                                                  {meetups.map(
+                                                      ({ id, speakers, title, event_speakers }) => {
+                                                          return (
+                                                              <Meetup
+                                                                  key={id}
+                                                                  speakers={speakers}
+                                                                  title={title}
+                                                                  event_speakers={event_speakers}
+                                                              />
+                                                          );
+                                                      },
+                                                  )}
                                               </MonthSection>
                                           );
                                       })}
