0c2e2bddcc0ffbc4fea2731f89366315ce2b5337
Emily Giurleo <e.m.giurleo@gmail.com>
2022-04-12 20:47:11 -0400

    Only run job board digest task on Mondays

--- spec/tasks/job_board/digest_spec.rb
+++ spec/tasks/job_board/digest_spec.rb
@@ -5,18 +5,35 @@ require 'rails_helper'
 Rails.application.load_tasks
 
 RSpec.describe 'job_board:digest' do
-  after(:each) do
-    Rake::Task['job_board:digest'].reenable
+  after(:each) { Rake::Task['job_board:digest'].reenable }
+
+  context 'when it is Monday' do
+    # April 11, 2022 was a Monday
+    before { Timecop.travel(Time.new(2022, 4, 11)) }
+    after { Timecop.return }
+
+    it 'posts job listings to Slack' do
+      client = stub_slack_client
+      job = create(:job)
+      Rake::Task['job_board:digest'].invoke
+
+      expect(client).to have_received(:message_channel).with(
+        hash_including(text: /- #{job.title} at #{job.company}/),
+      )
+    end
   end
 
-  it 'posts job listings to Slack' do
-    client = stub_slack_client
-    job = create(:job)
-    Rake::Task['job_board:digest'].invoke
+  context 'when it is not Monday' do
+    # April 12, 2022 was a Tuesday
+    before { Timecop.travel(Time.new(2022, 4, 12)) }
+    after { Timecop.return }
 
-    expect(client).to have_received(:message_channel).with(
-      hash_including(text: /- #{job.title} at #{job.company}/)
-    )
+    it 'does not post job listings to Slack' do
+      client = stub_slack_client
+      Rake::Task['job_board:digest'].invoke
+
+      expect(client).not_to have_received(:message_channel)
+    end
   end
 
   def stub_slack_client
