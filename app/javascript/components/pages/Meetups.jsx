51b1805d879b4a929cdd5a85948973cab17562fb
mayra lucia navarro <luciagirasoles@gmail.com>
2022-06-24 18:39:12 -0500

    Adjust aligment on cards - Past Meetups

--- app/javascript/components/pages/Meetups.jsx
+++ app/javascript/components/pages/Meetups.jsx
@@ -22,7 +22,7 @@ YearSection.propTypes = {
 
 const MonthSection = ({ children, month }) => (
     <li key={month} className="meetups__month flex flex-col items-start md:flex-row">
-        <div className="meetups__month--name w-36">
+        <div className="meetups__month--name w-32">
             <h3 className="py-1 px-2 mb-4 w-min border-2 border-red-400 rounded uppercase text-md text-red-400 md:ml-auto">
                 {month}
             </h3>
