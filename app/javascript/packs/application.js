f8c2bf43601232f8eba56c62f4cc31ccf7ea4a4a
mayra lucia navarro <luciagirasoles@gmail.com>
2022-12-14 18:15:37 -0500

    Removes double render call

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from '@rails/ujs';
+// import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 
 import '../stylesheets/application.scss';
 
-document.addEventListener('turbolinks:load', function () {
-    Rails.start();
-});
+// Rails.start();
 
 ActiveStorage.start();
