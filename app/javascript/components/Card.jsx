2ff44107537025ab641c28984eb7546182a3edb2
Angela Choi <angelamchoi@gmail.com>
2023-02-15 20:14:31 -0500

    removed code from card component and reverted to original code

--- app/javascript/components/Card.jsx
+++ app/javascript/components/Card.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'components/Button';
 
 import 'stylesheets/card';
 
-const Card = ({ children, className, link }) => (
-    <>
-        <div className={`card p-10 ${className}`}>
-            {children}
-            <div className="flex flex-row justify-between items-center space-x-5">
-                <Button type="white" className="w-20">
-                    <a href={link} target="_blank" rel="noopener noreferrer">
-                        Apply
-                    </a>
-                </Button>
-            </div>
-        </div>
-    </>
+const Card = ({ children, className }) => (
+    <div className={`card p-10 ${className}`}>{children} </div>
 );
 
 Card.propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
-    link: PropTypes.string,
+    link: PropTypes.boolean,
 };
 
 export default Card;
--- app/javascript/components/pages/Jobs.jsx
+++ app/javascript/components/pages/Jobs.jsx
@@ -105,7 +105,7 @@ const SponsorUsBanner = () => {
     );
 };
 
-const Job = ({ title, description, imageUrl, company, location }) => {
+const Job = ({ title, description, imageUrl, company, location, link }) => {
     return (
         <Card className="mx-0 my-5 md:mr-8 max-w-[22rem]">
             <div className="flex flex-row">
@@ -116,7 +116,14 @@ const Job = ({ title, description, imageUrl, company, location }) => {
                     <div className="font-light">{location}</div>
                 </div>
             </div>
-            <div className="my-5 h-44">{description}</div>
+            <div className="my-5 h-48">{description} </div>
+            <div className="flex flex-row justify-between items-center space-x-5">
+                <Button type="white" className="w-20">
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                        Apply
+                    </a>
+                </Button>
+            </div>
         </Card>
     );
 };
