53ac111644d99fad0e403b2443acf61e9149a683
mayra lucia navarro <luciagirasoles@gmail.com>
2023-04-05 02:24:04 -0500

    Config PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+let environment = {
     plugins: [
         require('tailwindcss'),
+        require('autoprefixer'),
         require('postcss-import'),
         require('postcss-flexbugs-fixes'),
         require('postcss-preset-env')({
@@ -11,3 +12,20 @@ module.exports = {
         }),
     ],
 };
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (['production'].includes(process.env.RAILS_ENV)) {
+    environment.plugins.push(
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './app/**/*.html.erb',
+                './app/helpers/**/*.rb',
+                './app/javascript/**/*.js',
+                './app/javascript/**/*.jsx',
+            ],
+            defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        }),
+    );
+}
+
+module.exports = environment;
