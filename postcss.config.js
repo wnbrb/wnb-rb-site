c71819b7b876f36d14885613e393bcc38c7e1ab5
mayra lucia navarro <luciagirasoles@gmail.com>
2023-04-06 03:55:06 -0500

    Fixes multiple reders

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,15 @@
-let environment = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        require('postcss-import'),
-        require('postcss-flexbugs-fixes'),
-        require('postcss-preset-env')({
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        'postcss-import': {},
+        'postcss-flexbugs-fixes': {},
+        'postcss-preset-env': {
             autoprefixer: {
                 flexbox: 'no-2009',
             },
             stage: 3,
-        }),
-    ],
+        },
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+    },
 };
-
-// Only run PurgeCSS in production (you can also add staging here)
-if (['production'].includes(process.env.RAILS_ENV)) {
-    environment.plugins.push(
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './app/**/*.html.erb',
-                './app/helpers/**/*.rb',
-                './app/javascript/**/*.js',
-                './app/javascript/**/*.jsx',
-            ],
-            defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-        }),
-    );
-}
-
-module.exports = environment;
