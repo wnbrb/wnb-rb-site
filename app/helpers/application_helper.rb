755842195ce55f0bcdd0df359454e65f68c9d69f
mayra lucia navarro <luciagirasoles@gmail.com>
2023-05-26 02:28:54 -0500

    Add meta description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
   def canonical_url
     request.base_url + request.path
   end
+
+  def meta_description
+    "WNB.rb is a virtual community for women and non-binary Rubyists. We offer a variety of resources and events to
+    help you learn, grow, and connect with other Rubyists. Whether you're a beginner or a seasoned pro, we have
+    something for you."
+  end
 end
