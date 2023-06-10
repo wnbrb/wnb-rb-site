669cf610a6cc51d4f04a592e45691d37fbbe6c47
mayra lucia navarro <luciagirasoles@gmail.com>
2023-06-09 23:23:14 -0500

    Revert changes on login

--- spec/system/admin_login_system_spec.rb
+++ spec/system/admin_login_system_spec.rb
@@ -50,10 +50,8 @@ RSpec.describe 'Admin login system', type: :system do
 
         click_on 'Sign out'
 
-        expect(page).to have_text('Signed out successfully')
-        expect(page).to have_current_path(new_user_session_path)
+        expect(page).to have_current_path(root_path)
       end
-
     end
 
     it 'enables me to reset WNB.rb admin password' do
