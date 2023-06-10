818894a335df187d2a523aefae8489c28f3e939a
mayra lucia navarro <luciagirasoles@gmail.com>
2023-06-09 23:15:33 -0500

    Fix lint issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
     admin_dashboard_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
 
