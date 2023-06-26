70ca0d13d72bf1d395832a3ff82a63e2b9942d5c
mayra lucia navarro <luciagirasoles@gmail.com>
2023-06-25 23:07:12 -0500

    Add unit tests for Event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:event_speakers).dependent(:destroy) }
+    it { is_expected.to have_many(:speakers).through(:event_speakers) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:location) }
+    it { is_expected.to validate_presence_of(:date) }
+  end
 end
