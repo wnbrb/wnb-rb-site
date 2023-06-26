87baa75c463d89b3326c53037a46d875eebc1601
mayra lucia navarro <luciagirasoles@gmail.com>
2023-06-25 23:10:44 -0500

    Add unit tests for Speaker

--- spec/models/speaker_spec.rb
+++ spec/models/speaker_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Speaker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:event_speakers).dependent(:destroy) }
+    it { is_expected.to have_many(:events).through(:event_speakers) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:bio) }
+    it { is_expected.to validate_presence_of(:image_url) }
+  end
 end
