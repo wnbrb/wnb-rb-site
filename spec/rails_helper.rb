e75cb3ed32f0fa0c2057ba88719a8e479cea3f09
Mayra Lucia Navarro <luciagirasoles@gmail.com>
2023-10-11 21:56:17 -0500

    move devise helpers into support file (#196)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,9 +70,6 @@ RSpec.configure do |config|
 
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
 end
 
 Shoulda::Matchers.configure do |config|
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 RSpec.configure do |config|
   config.include Warden::Test::Helpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
