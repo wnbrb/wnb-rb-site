86cfff14717666d398cfd40e699113aa4ca1e342
mayra lucia navarro <luciagirasoles@gmail.com>
2023-10-11 22:08:10 -0500

    Move code to new file for improved readability

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,12 +8,6 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'factory_bot_rails'
-require 'rspec/mocks'
-
-# FactoryBot.factories.clear
-# FactoryBot.reload
-FactoryBot::SyntaxRunner.class_eval { include RSpec::Mocks::ExampleMethods }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -70,11 +64,4 @@ RSpec.configure do |config|
 
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+require 'factory_bot_rails'
+require 'rspec/mocks'
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+end
+
+FactoryBot::SyntaxRunner.class_eval do 
+  include RSpec::Mocks::ExampleMethods
 end
