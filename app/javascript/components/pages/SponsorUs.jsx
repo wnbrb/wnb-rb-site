db764977a9759209ac1c53db54d7f2b322c30037
mayra lucia navarro <luciagirasoles@gmail.com>
2024-06-23 21:38:33 -0500

    Add background color to second section Sponsor Us page

--- app/javascript/components/pages/SponsorUs.jsx
+++ app/javascript/components/pages/SponsorUs.jsx
@@ -13,22 +13,20 @@ const SponsorUs = () => (
 
         <SharedLayout>
             <PageTitleWithContainer text="Sponsor Us" />
-            <div className="container flex flex-col mx-auto md:max-w-[50rem] lg:max-w-[60rem]">
-                <section className="sponsorship-options">
-                    <h2 className="text-2xl font-bold my-2">Sponsorship tiers</h2>
+            <section className="container flex flex-col mx-auto md:max-w-[50rem] lg:max-w-[60rem] sponsorship-options">
+                <h2 className="px-10 text-2xl font-bold my-2">Sponsorship tiers</h2>
 
-                    <stripe-pricing-table
-                        pricing-table-id="prctbl_1PSlcMBHbLqUjvnWol3sB6vA"
-                        publishable-key="pk_live_51JmdS1BHbLqUjvnWRLnx9KBZtHWLWHe67N3OpsV0FERko2K4lAZ6oyEo4pIq84YIMQPUJ8K1FY1UevXjZZPsiloq00EYai7JBQ"
-                    />
-                </section>
+                <stripe-pricing-table
+                    pricing-table-id="prctbl_1PSlcMBHbLqUjvnWol3sB6vA"
+                    publishable-key="pk_live_51JmdS1BHbLqUjvnWRLnx9KBZtHWLWHe67N3OpsV0FERko2K4lAZ6oyEo4pIq84YIMQPUJ8K1FY1UevXjZZPsiloq00EYai7JBQ"
+                />
+            </section>
 
-                <hr className="my-12 mx-48" />
-
-                <div className="flex flex-row justify-between items-center space-x-5 mb-12 mt-2">
-                    <Card className="max-w-[35rem] min-h-[17rem]">
+            <section className="mt-12 lg:mt-28 bg-wnbrb-pink-light">
+                <div className="container mx-auto flex flex-col md:flex-row gap-8 xl:gap-16 py-20 px-10 lg:px-8 xl:px-12">
+                    <Card className="w-100 md:w-1/2 flex-1 flex flex-col">
                         <h2 className="text-2xl font-bold mb-2">Hire from WNB.rb</h2>
-                        <p className="mb-8">
+                        <p className="mb-8 grow">
                             Even if you aren&apos;t a sponsor, you can hire from our community by
                             posting your open jobs on our job board.
                         </p>
@@ -37,15 +35,15 @@ const SponsorUs = () => (
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <Button type="primary" className="max-w-[8rem]">
+                            <Button type="primary" className="max-w-[8rem] ">
                                 Post a job
                             </Button>
                         </a>
                     </Card>
 
-                    <Card className="max-w-[30rem] min-h-[17rem]">
+                    <Card className="w-100 md:w-1/2 flex-1 flex flex-col">
                         <h2 className="text-2xl font-bold mb-2">Make a donation</h2>
-                        <p className="mb-8">
+                        <p className="mb-8 grow">
                             Not ready to sponsor WNB.rb? That&apos;s okay! We&apos;d still greatly
                             appreciate a donation of any amount. All the money we earn goes back
                             into the community.
@@ -61,7 +59,7 @@ const SponsorUs = () => (
                         </a>
                     </Card>
                 </div>
-            </div>
+            </section>
         </SharedLayout>
     </>
 );
