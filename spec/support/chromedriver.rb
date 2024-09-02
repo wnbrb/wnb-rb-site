ead1f48b1047cc9d03c9113c77148b8d2854ebe5
mayra lucia navarro <luciagirasoles@gmail.com>
2024-09-02 00:46:57 -0500

    Update chrome headless options

--- spec/support/chromedriver.rb
+++ spec/support/chromedriver.rb
@@ -4,14 +4,11 @@ require 'selenium-webdriver'
 # Next line turn on logging to see what Selenium Manager is doing, in case of errors
 # Selenium::WebDriver.logger.level = :debug
 
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument '--window-size=1680,1050'
-  options.add_argument '--headless'
+  options.add_argument '--headless=new'
   # options.add_argument '--no-sandbox'
   options.add_argument '--disable-gpu'
   options.add_argument '--disable-dev-shm-usage'
@@ -25,7 +22,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) { driven_by Capybara.javascript_driver }
 
   config.after(:each, type: :system) do
-    Capybara.reset_sessions!
+    # Capybara.reset_sessions!
     page.driver.reset!
   end
 end
