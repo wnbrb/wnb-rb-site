59c826c906d130798c0bbc9513b2bcae65f601bd
fatmahussein <fartunhussein346@gmail.com>
2024-11-29 13:24:42 +0300

    Remove the texts

--- app/javascript/components/PageTitle.jsx
+++ app/javascript/components/PageTitle.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import RubyOutline from 'components/icons/RubyOutline';
+import RubyOutline from 'components/icons/NewLogo';
 import 'stylesheets/page-title';
 
-const PageTitle = ({ text, altText, children }) => {
+const PageTitle = ({ children }) => {
     return (
         <div className="page-title">
             <RubyOutline className="ruby-outline" />
-            <div className="title-container">
-                <h1 className="title-text" alt={altText}>
-                    {text}
-                </h1>
+                    
                 {children}
-            </div>
+          
         </div>
     );
 };
 
 PageTitle.propTypes = {
     children: PropTypes.node,
-    text: PropTypes.string,
-    altText: PropTypes.string,
+   
 };
 
 export default PageTitle;
