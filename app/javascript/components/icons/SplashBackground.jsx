fb36b7e4b5a0bc0c6f4c5a09d1144de9e83b3312
fatmahussein <fartunhussein346@gmail.com>
2024-12-17 14:23:43 +0300

    update the images in the splash background

--- app/javascript/components/icons/SplashBackground.jsx
+++ app/javascript/components/icons/SplashBackground.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import us from '../../../assets/images/us.png';
+
 import pin from '../../../assets/images/pin.svg';
-import wnb from '../../../assets/images/wnb.svg';
+
 import colors from '../../../assets/images/colors.svg';
+
+import two from '../../../assets/images/two.jpg';
+import three from '../../../assets/images/three.jpg';
 
 const SplashBackground = ({ className }) => (
     <div className={`grid grid-cols-2 gap-0 ${className}`}>
         <div className="w-full bg-gray-200 flex items-center justify-center">
-            <img src={wnb} alt="Image 1" className="object-cover w-full h-full" />
+            <img src={two} alt="Image 1" className="object-cover w-full h-full" />
         </div>
         <div className="w-full bg-gray-200 flex items-center justify-center">
             <img src={pin} alt="Image 2" className="object-cover w-full h-full" />
@@ -17,7 +20,7 @@ const SplashBackground = ({ className }) => (
             <img src={colors} alt="Image 3" className="object-cover w-full h-full" />
         </div>
         <div className="w-full bg-gray-200 flex items-center justify-center">
-            <img src={us} alt="Image 4" className="object-cover w-full h-full" />
+            <img src={three} alt="Image 4" className="object-cover w-full h-full" />
         </div>
     </div>
 );
