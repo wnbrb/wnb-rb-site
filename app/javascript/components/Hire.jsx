b42a0628c6347acea9e501ef1cdb9fcb051c12b0
fatmahussein <fartunhussein346@gmail.com>
2024-12-20 16:18:14 +0300

    Update the image in hire section

--- app/javascript/components/Hire.jsx
+++ app/javascript/components/Hire.jsx
@@ -1,34 +1,27 @@
 import React from 'react';
-import hire from '../../assets/images/hire.svg';
 import Button from 'components/Button';
+import four from '../../assets/images/four.jpg';
 import 'stylesheets/hire';
 
 const Hire = () => {
     return (
         <div className="hire-layout">
             <div className="hire w-100 md:w-1/2 flex-1 flex flex-col">
-                <img src={hire} alt="Hire" />
+                <img className="hire-img" src={four} alt="Hire-from-wnb-rb" />
                 <h2 className="text-2xl font-bold mb-2">Hire from WNB.rb</h2>
                 <p className="mb-8 grow">
                     Even if you aren&apos;t a sponsor, you can hire from our community by posting
                     your open jobs on our job board.
                 </p>
-                <a
-                    href="https://buy.stripe.com/28o4gW9cZ7oKbwQ7sJ"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                <Button
+                    type="primary"
+                    className="max-w-[8rem]"
+                    onClick={() => {
+                        window.open('mailto:sarah@wnb-rb.dev', '_blank', 'noopener noreferrer');
+                    }}
                 >
-                    <Button type="primary" className="max-w-[8rem] ">
-                        <a
-                            href="mailto:sarah@wnb-rb.dev"
-                            aria-label="Email"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Post a job
-                        </a>
-                    </Button>
-                </a>
+                    Post a job
+                </Button>
             </div>
         </div>
     );
