76f446f6ef6f078b2aea1bf8a3db34fbb5c055e4
fatmahussein <fartunhussein346@gmail.com>
2024-12-20 19:27:09 +0300

    Update the file to it's original format

--- app/javascript/components/PageTitle.jsx
+++ app/javascript/components/PageTitle.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import RubyOutline from 'components/icons/NewLogo';
+import NewLogo from 'components/icons/NewLogo';
 import 'stylesheets/page-title';
 
-const PageTitle = ({ children }) => {
+const PageTitle = ({ text, altText, children  }) => {
     return (
         <div className="page-title">
-            <RubyOutline className="ruby-outline" />
-
-            {children}
+            <NewLogo className="new-logo" />
+            <div className="title-container">
+                <h1 className="title-text" alt={altText}>
+                    {text}
+                </h1>
+                {children}
+            </div>
+     
         </div>
     );
 };
 
 PageTitle.propTypes = {
     children: PropTypes.node,
+    text: PropTypes.string,
+    altText: PropTypes.string,
 };
 
 export default PageTitle;
