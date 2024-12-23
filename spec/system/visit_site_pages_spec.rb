f58f313cd432568102c9435343507e28f2123a18
Sarah Eggleston <segglestn@gmail.com>
2024-12-23 10:35:47 +0000

    Reinstate specs

--- spec/system/visit_site_pages_spec.rb
+++ spec/system/visit_site_pages_spec.rb
@@ -7,69 +7,69 @@ RSpec.describe 'User visit site pages', type: :system, js: true do
     stub_const('ENV', ENV.to_hash.merge('JOB_BOARD_PASSWORD' => 'testing'))
     stub_const('ENV', ENV.to_hash.merge('JWT_HMAC_SECRET' => 'testing1'))
   end
-  # it 'visits home page' do
-  #   visit root_path
-  #   expect(page).to have_text('A virtual community for women and non-binary Rubyists')
-  # end
+  it 'visits home page' do
+    visit root_path
+    expect(page).to have_text('A virtual community for women and non-binary Rubyists')
+  end
 
-  # it 'visits join_us page' do
-  #   visit join_us_path
-  #   expect(page).to have_text('Join WNB.rb!')
-  # end
+  it 'visits join_us page' do
+    visit join_us_path
+    expect(page).to have_text('Join WNB.rb!')
+  end
 
-  # it 'visits meetups page' do
-  #   visit meetups_path
-  #   expect(page).to have_text('Meetups')
-  # end
+  it 'visits meetups page' do
+    visit meetups_path
+    expect(page).to have_text('Meetups')
+  end
 
-  # it 'visits past meetup today' do
-  #   meetup = create(:event, date: Date.today - 30.days)
+  it 'visits past meetup today' do
+    meetup = create(:event, date: Date.today - 30.days)
 
-  #   visit "/meetups/#{meetup.date.year}/#{meetup.date.month}/#{meetup.date.day}"
+    visit "/meetups/#{meetup.date.year}/#{meetup.date.month}/#{meetup.date.day}"
 
-  #   expect(page).to have_text(meetup.title)
-  # end
+    expect(page).to have_text(meetup.title)
+  end
 
-  # it 'visits past meetup and displays speaker with valid links' do
-  #   meetup = create(:event, date: Date.new(2024, 2, 17), title: 'Meetup February 2024')
-  #   speaker = create(:speaker, :with_valid_links)
-  #   speaker2 = create(:speaker, :with_valid_links, links: {mastodon: Faker::Internet.url})
-  #   create(:talk, speaker: speaker, event: meetup)
-  #   create(:talk, speaker: speaker2, event: meetup)
+  it 'visits past meetup and displays speaker with valid links' do
+    meetup = create(:event, date: Date.new(2024, 2, 17), title: 'Meetup February 2024')
+    speaker = create(:speaker, :with_valid_links)
+    speaker2 = create(:speaker, :with_valid_links, links: {mastodon: Faker::Internet.url})
+    create(:talk, speaker: speaker, event: meetup)
+    create(:talk, speaker: speaker2, event: meetup)
 
-  #   visit "/meetups/#{meetup.date.year}/#{meetup.date.month}/#{meetup.date.day}"
+    visit "/meetups/#{meetup.date.year}/#{meetup.date.month}/#{meetup.date.day}"
 
-  #   expect(page).to have_link(href: speaker2.links['mastodon'].to_s, exact: true)
-  #   expect(meetup.speakers.count).to eq(2)
-  # end
+    expect(page).to have_link(href: speaker2.links['mastodon'].to_s, exact: true)
+    expect(meetup.speakers.count).to eq(2)
+  end
 
-  # it 'visits past meetup and displays available valid links speaker only' do
-  #   meetup = create(:event, date: Date.new(2024, 2, 17), title: 'Meetup February 2024')
-  #   speaker = create(:speaker, links: { mastodon: Faker::Internet.url })
-  #   create(:talk, speaker: speaker, event: meetup)
+  it 'visits past meetup and displays available valid links speaker only' do
+    meetup = create(:event, date: Date.new(2024, 2, 17), title: 'Meetup February 2024')
+    speaker = create(:speaker, links: { mastodon: Faker::Internet.url })
+    create(:talk, speaker: speaker, event: meetup)
 
-  #   visit "/meetups/#{meetup.date.year}/#{meetup.date.month}/#{meetup.date.day}"
+    visit "/meetups/#{meetup.date.year}/#{meetup.date.month}/#{meetup.date.day}"
 
-  #   expect(page).not_to have_link(href: speaker.links['twitter'].to_s, exact: true)
-  #   expect(meetup.speakers.count).to eq(1)
-  # end
+    expect(page).not_to have_link(href: speaker.links['twitter'].to_s, exact: true)
+    expect(meetup.speakers.count).to eq(1)
+  end
 
-  # context 'visit to unknown path' do
-  #   before do
-  #     method = Rails.application.method(:env_config)
-  #     allow(Rails.application).to receive(:env_config).with(no_args) do
-  #       method.call.merge(
-  #         'action_dispatch.show_exceptions' => true,
-  #         'action_dispatch.show_detailed_exceptions' => false,
-  #         'consider_all_requests_local' => false
-  #       )
-  #     end
-  #   end
+  context 'visit to unknown path' do
+    before do
+      method = Rails.application.method(:env_config)
+      allow(Rails.application).to receive(:env_config).with(no_args) do
+        method.call.merge(
+          'action_dispatch.show_exceptions' => true,
+          'action_dispatch.show_detailed_exceptions' => false,
+          'consider_all_requests_local' => false
+        )
+      end
+    end
 
-  #   it 'responds with not found page' do
-  #     visit '/nonexistentpath'
+    it 'responds with not found page' do
+      visit '/nonexistentpath'
 
-  #     expect(page).to have_text('404')
-  #   end
-  # end
+      expect(page).to have_text('404')
+    end
+  end
 end
