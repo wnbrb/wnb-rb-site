a55eb0931c705a2f497165c004952265ba5853d3
fatmahussein <fartunhussein346@gmail.com>
2025-01-29 16:56:33 +0300

    Modified to match new design

--- app/javascript/components/pages/Meetups.jsx
+++ app/javascript/components/pages/Meetups.jsx
@@ -1,125 +1,99 @@
 import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { Helmet } from 'react-helmet-async';
 import { getPastMeetups } from '../../datasources';
 import SharedLayout from 'components/layout/SharedLayout';
 import LoadingSpinner from 'components/LoadingSpinner';
-import PageTitleWithContainer from 'components/PageTitleWithContainer';
 import Meetup from '../meetups/MeetUp';
-import YearSection from '../meetups/YearSection';
-import MonthSection from '../meetups/MonthSection';
 
 import 'stylesheets/meetup';
+import EventsCalendar from '../meetups/EventsCalendar';
+import horizontal from '../../../assets/images/horizontal-line.svg';
 
 const Meetups = () => {
     const [loading, setLoading] = useState(true);
-    const [meetupsByYear, setMeetupsByYear] = useState({});
+    const [meetups, setMeetups] = useState([]);
+    const [visibleCount, setVisibleCount] = useState(3);
+
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)' });
+
     useEffect(() => {
         const fetchData = async () => {
             const data = await getPastMeetups();
-            setMeetupsByYear(Object.entries(data));
+            const flattenedMeetups = Object.entries(data).flatMap(([year, meetupsByMonth]) => {
+                return Object.values(meetupsByMonth).flat();
+            });
+            
+            setMeetups(flattenedMeetups);
             setLoading(false);
         };
 
         fetchData();
     }, []);
 
+    useEffect(() => {
+        setVisibleCount(isMobile ? 2 : 3);
+    }, [isMobile]);
+
+    const handleSeeMore = () => {
+        setVisibleCount(meetups.length);
+    };
+
+    const visibleMeetups = meetups.slice(0, visibleCount);
+
     return (
         <>
             <Helmet>
                 <title>Meetups | WNB.rb</title>
             </Helmet>
+            <EventsCalendar events={meetups} />
+            <img src={horizontal} className="line" alt="horizontal line" />
             <SharedLayout>
-                <PageTitleWithContainer text="Meetups"></PageTitleWithContainer>
-                <div className="max-w-[73rem] p-10 mx-auto bg-gray-100">
-                    <p className="mb-2">
-                        <b>Our meetups</b> are held virtually on the last Tuesday of every month at{' '}
-                        <a
-                            href="https://dateful.com/convert/eastern-time-et?t=12pm"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            12 pm Eastern Time
-                        </a>{' '}
-                        <i className="bi bi-box-arrow-up-right"></i>. The content is typically
-                        focused on Ruby or career-related topics, with 1-2 talks presented in a
-                        lecture-style format. Anyone who identifies as a woman or non-binary
-                        individual is welcome to attend. Fill in{' '}
-                        <a
-                            href="/join-us"
-                            className="whitespace-nowrap font-medium hover:text-red-400"
-                        >
-                            the Subscription form
-                        </a>{' '}
-                        to receive an invitation to the Google Group, which will opt you into
-                        calendar invites for upcoming meetups.
-                    </p>
-                    <p>
-                        <b>Are you interested in being a speaker?</b>
-                        {` We're constantly on the
-                        lookout for presenters, whether you're an experienced speaker or if this is
-                        your first time. Speaking at a WNB.rb meetup is an excellent opportunity to
-                        enhance your public speaking abilities. If you're interested in giving a
-                        talk, please provide your talk details via `}
-                        <a
-                            href="https://docs.google.com/forms/d/e/1FAIpQLSdmOJxLdSdy74mXYxsr6ZRRhTN95Yxnq2B6n5mhUoGkVmDUGA/viewform"
-                            className="whitespace-nowrap font-medium hover:text-red-400"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            the Talk Proposal form
-                        </a>{' '}
-                        <i className="bi bi-box-arrow-up-right"></i> or feel free to contact{' '}
-                        <i>Adrianna Chang</i> on the WNB.rb Slack for more information.
-                    </p>
-                </div>
                 {loading ? (
                     <LoadingSpinner />
                 ) : (
-                    <div className="container flex flex-col mx-auto md:max-w-[50rem] lg:max-w-[73rem]">
-                        {meetupsByYear.length > 0
-                            ? meetupsByYear.reverse().map(([year, meetupsByMonth]) => {
-                                  return (
-                                      <YearSection key={year} year={year}>
-                                          {Object.entries(meetupsByMonth).map(
-                                              ([month, meetups]) => {
-                                                  return (
-                                                      <MonthSection key={month} month={month}>
-                                                          {meetups.map(
-                                                              ({
-                                                                  id,
-                                                                  speakers,
-                                                                  title,
-                                                                  date,
-                                                                  talks,
-                                                              }) => {
-                                                                  const dateString =
-                                                                      date.split('T')[0];
-                                                                  let [year, month, day] =
-                                                                      dateString.split('-');
-                                                                  return (
-                                                                      <Meetup
-                                                                          key={id}
-                                                                          speakers={speakers}
-                                                                          title={title}
-                                                                          talks={talks}
-                                                                          year={year}
-                                                                          month={month.padStart(
-                                                                              2,
-                                                                              '0',
-                                                                          )}
-                                                                          day={day}
-                                                                      />
-                                                                  );
-                                                              },
-                                                          )}
-                                                      </MonthSection>
-                                                  );
-                                              },
-                                          )}
-                                      </YearSection>
-                                  );
-                              })
-                            : null}
+                    <div className='meetup-section-container'>
+                    <div className="container flex justify-center flex-col mx-auto ">
+                        <div className="flex flex-col gap-2 justify-center items-center mx-auto ">
+                            <h5 className="font-rubik text-xl font-bold text-wnbrb-blue-navy ">Archive</h5>
+                            <h1 className="font-besley text-3xl font-bold text-wnbrb-blue-navy ">Past Meetups</h1>
+                        </div>
+
+                        <div className="archive ">
+                            {visibleMeetups.length > 0 ? (
+                                <>
+                                    {visibleMeetups.map(({ id, speakers, title, date, talks }) => {
+                                        const dateString = date.split('T')[0];
+                                        const [year, month, day] = dateString.split('-');
+
+                                        return (
+                                            <Meetup
+                                                key={id}
+                                                speakers={speakers}
+                                                title={title}
+                                                talks={talks}
+                                                year={year}
+                                                month={month.padStart(2, '0')}
+                                                day={day}
+                                            />
+                                        );
+                                    })}
+                                </>
+                            ) : (
+                                <p>No meetups available.</p>
+                            )}
+                        </div>
+                        <div className="flex justify-center items-center">
+                            {visibleCount < meetups.length && (
+                                <button
+                                    className="see-more-btn mt-6 px-2 py-3 bg-wnbrb-blue-navy  font-bold"
+                                    onClick={handleSeeMore}
+                                >
+                                    See More
+                                </button>
+                            )}
+                        </div>
+                    </div>
                     </div>
                 )}
             </SharedLayout>
