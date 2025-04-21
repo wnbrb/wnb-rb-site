a929cb6fc9197c3644e14c63b1ffa554e5f38310
Jennifer Russell <jc.russell@yahoo.com>
2025-04-21 16:31:22 -0400

    Fix Rubocop offenses

---
+++ spec/tasks/user/add_user_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require 'rails_helper'
+require 'rake'
+
+RSpec.describe 'user:add_user' do
+
+context 'admin setup via rake' do
+  before(:all) do
+    Rake.application.rake_require('tasks/new_admin')
+    Rake::Task.define_task(:environment)
+  end
+
+  it 'runs the rake task to add a user' do
+
+    expect do
+      Rake::Task['user:add_user'].reenable
+      Rake::Task['user:add_user'].invoke('Admin', 'test@example.com', 'changeme', 'changeme', 'admin')
+      end.to change(User, :count).by(1)
+
+  end
+end
+
+end
