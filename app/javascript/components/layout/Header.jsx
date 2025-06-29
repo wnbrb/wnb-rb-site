51c959283731eda5b3555d69b689be1ba2c945cc
fatmahussein <fartunhussein346@gmail.com>
2025-06-29 17:45:09 +0300

    Add “Our Community” to site navigation linking to /community

--- app/javascript/components/layout/Header.jsx
+++ app/javascript/components/layout/Header.jsx
@@ -6,9 +6,10 @@ import 'stylesheets/header.scss';
 const Header = () => {
     const links = [
         { id: 1, text: 'Events', href: '/meetups' },
-        { id: 2, text: 'Donate', href: 'https://buy.stripe.com/6oE7t874ReRc7gA9AN' },
-        { id: 3, text: 'Support us', href: '/sponsor-us' },
-        { id: 4, text: 'Join', href: '/join-us' },
+        { id: 2, text: 'Our Community', href: '/community' },
+        { id: 3, text: 'Donate', href: 'https://buy.stripe.com/6oE7t874ReRc7gA9AN' },
+        { id: 4, text: 'Support us', href: '/sponsor-us' },
+        { id: 5, text: 'Join', href: '/join-us' },
     ];
 
     const [headerState, setHeaderState] = useState({
