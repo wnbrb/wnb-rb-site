d7d3f3de50e94de6e35f9bf4a7c7e01e331d6835
Emily Samp <emily.samp@icloud.com>
2025-06-29 13:28:36 -0500

    Simplify the frontend of the Join Us page

--- app/javascript/components/pages/JoinUs.jsx
+++ app/javascript/components/pages/JoinUs.jsx
@@ -20,18 +20,12 @@ const JoinUs = () => {
     const initialValues = {
         name: '',
         email: '',
-        joindiscord: '',
-        joinGoogleGroup: '',
         identifyAs: false,
-        currentJob: '',
-        lookingForJob: '',
     };
 
     const validationSchema = Yup.object({
         name: Yup.string().required('Name is required'),
         email: Yup.string().email('Invalid email address').required('Email is required'),
-        joindiscord: Yup.string().required('This is a required question'),
-        joinGoogleGroup: Yup.string().required('This is a required question'),
         identifyAs: Yup.boolean()
             .required('This is a required question')
             .oneOf([true], 'This is a required question'),
@@ -84,7 +78,7 @@ const JoinUs = () => {
                                 resetForm();
                                 setShowBanner({
                                     type: 'success',
-                                    message: `${values.name}, thank you for joining WNB.rb! You will receive an email within the next week.`,
+                                    message: `${values.name}, thank you for joining WNB.rb! You will receive an email inviting you to our Discord server shortly.`,
                                 });
                                 window.scrollTo({ top: 0, behavior: 'smooth' });
                             }
@@ -126,7 +120,7 @@ const JoinUs = () => {
                     />
                 )}
                 <div className="join-us-form-container">
-                    <PageTitle text="Wnb.rb" altText="Women and Non-Binary Rubyists">
+                    <PageTitle text="WNB.rb" altText="Women and Non-Binary Rubyists">
                         <p className="mt-8 font-syne text-base font-semibold">
                             A virtual community for women & non-binary Rubyists.
                         </p>
@@ -134,13 +128,9 @@ const JoinUs = () => {
                     <Card className="w-full max-w-[50rem] mt-5">
                         <h1 className="join text-black font-syne text-2xl">Join</h1>
                         <p className="mb-4">
-                            By filling out this form, you give the organizers of WNB.rb permission
-                            to add you to our discord workspace and/or Google Group.
-                        </p>
-                        <p className="mb-4">
-                            WNB.rb will not share your email with other community members or
-                            third-party organizations. Your membership in the Google Group will
-                            remain hidden from everyone except the WNB.rb organizers. See our{' '}
+                            By filling out this form, you are requesting an invite to the WNB.rb Discord server. {' '}
+                            WNB.rb will only use your email to send you an invite link; your information will never
+                            be shared without your consent. For more information see our{' '}
                             <a
                                 href="https://tinyurl.com/wnb-rb-coc"
                                 className="whitespace-nowrap font-medium hover:text-wnbrb-blue-navy"
@@ -149,10 +139,8 @@ const JoinUs = () => {
                             >
                                 code of conduct
                             </a>
-                            .
-                        </p>
-                        <p className="mb-4">
-                            Learn more{' '}
+                            {' '}or{' '}
+                            learn more{' '}
                             <a
                                 href="/"
                                 className="whitespace-nowrap font-medium hover:text-wnbrb-blue-navy"
@@ -161,7 +149,6 @@ const JoinUs = () => {
                             </a>
                             .
                         </p>
-
                         <Formik {...{ initialValues, validationSchema, onSubmit }}>
                             {({ isSubmitting, isValid }) => (
                                 <Form>
@@ -189,110 +176,6 @@ const JoinUs = () => {
                                             className="error-message"
                                         />
                                     </div>
-                                    <fieldset role="group">
-                                        <legend className="font-besley">
-                                            Would you like to be added to our discord workspace?
-                                        </legend>
-                                        <p className="input-description">
-                                            discord is WNB.rb‘s primary means of communication. It
-                                            is an awesome community of over 350 Rubyists and
-                                            includes channels for advice, technical help, interview
-                                            prep, and much more!
-                                        </p>
-                                        <label
-                                            htmlFor="joindiscord_yes"
-                                            className="label-radiobutton"
-                                        >
-                                            <Field
-                                                type="radio"
-                                                id="joindiscord_yes"
-                                                name="joindiscord"
-                                                value="Yes"
-                                            />
-                                            Yes
-                                        </label>
-                                        <label
-                                            htmlFor="joindiscord_no"
-                                            className="label-radiobutton"
-                                        >
-                                            <Field
-                                                type="radio"
-                                                id="joindiscord_no"
-                                                name="joindiscord"
-                                                value="No"
-                                            />
-                                            No
-                                        </label>
-                                        <label
-                                            htmlFor="joindiscord_already_in"
-                                            className="label-radiobutton"
-                                        >
-                                            <Field
-                                                type="radio"
-                                                id="joindiscord_already_in"
-                                                name="joindiscord"
-                                                value="Already in WNB.rb discord"
-                                            />
-                                            Already in WNB.rb discord
-                                        </label>
-                                        <ErrorMessage
-                                            name="joindiscord"
-                                            component="p"
-                                            className="error-message"
-                                        />
-                                    </fieldset>
-
-                                    <fieldset role="group">
-                                        <legend className="font-besley">
-                                            Would you like to be added to our Google Group?
-                                        </legend>
-                                        <p className="input-description">
-                                            This allows us to send you the invitation for our
-                                            monthly meetups featuring two technical talks and
-                                            breakout rooms.
-                                        </p>
-                                        <label
-                                            htmlFor="joinGoogleGroup_yes"
-                                            className="label-radiobutton"
-                                        >
-                                            <Field
-                                                type="radio"
-                                                id="joinGoogleGroup_yes"
-                                                name="joinGoogleGroup"
-                                                value="Yes"
-                                            />
-                                            Yes
-                                        </label>
-                                        <label
-                                            htmlFor="joinGoogleGroup_no"
-                                            className="label-radiobutton"
-                                        >
-                                            <Field
-                                                type="radio"
-                                                id="joinGoogleGroup_no"
-                                                name="joinGoogleGroup"
-                                                value="No"
-                                            />
-                                            No
-                                        </label>
-                                        <label
-                                            htmlFor="joinGoogleGroup_already_in"
-                                            className="label-radiobutton"
-                                        >
-                                            <Field
-                                                type="radio"
-                                                id="joinGoogleGroup_already_in"
-                                                name="joinGoogleGroup"
-                                                value="Already in WNB.rb Google Group"
-                                            />
-                                            Already in WNB.rb Google Group
-                                        </label>
-                                        <ErrorMessage
-                                            name="joinGoogleGroup"
-                                            component="p"
-                                            className="error-message"
-                                        />
-                                    </fieldset>
 
                                     <fieldset role="group">
                                         <legend className="font-besley ">
@@ -315,6 +198,7 @@ const JoinUs = () => {
                                             >
                                                 code of conduct
                                             </a>
+                                            .
                                         </p>
                                         <label htmlFor="identifyAs" className="label-checkbox">
                                             <Field
@@ -331,36 +215,6 @@ const JoinUs = () => {
                                             className="error-message"
                                         />
                                     </fieldset>
-
-                                    <h2 className="font-besley text-2xl mb-4">About Your Work</h2>
-                                    <p className="mb-8">
-                                        The following questions are about your work. Only the WNB.rb
-                                        organizers will use this information. We use it to better
-                                        understand the needs of community members, and how we can
-                                        support them. You are not required to answer them.
-                                    </p>
-
-                                    <div className="form-group">
-                                        <label htmlFor="currentJob" className="font-besley">
-                                            Where do you currently work?
-                                        </label>
-                                        <p className="input-description">
-                                            If you‘re self-employed, unemployed, or a student,
-                                            please let us know as well!
-                                        </p>
-                                        <Field name="currentJob" type="text" id="currentJob" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="lookingForJob" className="font-besley">
-                                            Are you currently looking for a job, or do you
-                                            anticipate looking for a job in the next 6 months?
-                                        </label>
-                                        <Field
-                                            name="lookingForJob"
-                                            type="text"
-                                            id="lookingForJob"
-                                        />
-                                    </div>
 
                                     <Button
                                         type="primary"
