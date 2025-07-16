e43ca6f4a4179489ea3ef06a96fb4cbd14bfec55
Emily Samp <emily.samp@icloud.com>
2025-07-16 13:07:19 -0500

    Add necessary arguments to the Discord invite email preview

--- spec/mailers/previews/discord_invitation_mailer_preview.rb
+++ spec/mailers/previews/discord_invitation_mailer_preview.rb
@@ -3,7 +3,7 @@ class DiscordInvitationMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/discord_invitation_mailer_mailer/invite
   def invite
-    DiscordInvitationMailer.invite
+    DiscordInvitationMailer.invite('Emily', 'emily@wnb-rb.dev', 'INVITE LINK HERE')
   end
 
 end
