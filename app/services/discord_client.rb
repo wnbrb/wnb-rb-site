2b9a191419a98cfaa6b3456a168196fdbd58359a
fatmahussein <fartunhussein346@gmail.com>
2025-07-18 12:49:08 +0300

    implement DiscordClient to send messages via webhook

---
+++ app/services/discord_client.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require 'net/http'
+require 'json'
+require 'uri'
+
+class DiscordClient
+  WEBHOOK_URL = ENV.fetch('DISCORD_WEBHOOK_URL')
+
+  def self.message(text)
+    uri = URI.parse(WEBHOOK_URL)
+    payload = { content: text }
+    headers = { 'Content-Type' => 'application/json' }
+
+    response = Net::HTTP.post(uri, payload.to_json, headers)
+    Rails.logger.info "Discord webhook sent: #{response.code} #{response.body}"
+  end
+end
