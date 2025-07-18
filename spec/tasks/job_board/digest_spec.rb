039e6bc7909472816b651f29b340a3f70c108bb2
fatmahussein <fartunhussein346@gmail.com>
2025-07-18 13:00:59 +0300

    Add tests for job_board:digest rake task posting to Discord

--- spec/tasks/job_board/digest_spec.rb
+++ spec/tasks/job_board/digest_spec.rb
@@ -5,19 +5,27 @@ require 'rails_helper'
 Rails.application.load_tasks
 
 RSpec.describe 'job_board:digest' do
+
+   around do |example|
+    ClimateControl.modify(DISCORD_WEBHOOK_URL: 'https://example.com/webhook') do
+      example.run
+    end
+  end
+
   after(:each) { Rake::Task['job_board:digest'].reenable }
+
 
   context 'when it is Monday' do
     before { travel_to(Time.now.last_week(:monday)) }
     after { travel_back }
 
-    it 'posts job listings to Slack' do
-      client = stub_slack_client
+    it 'posts job listings to Discord' do
+      client = stub_discord_client
       job = create(:job)
       Rake::Task['job_board:digest'].invoke
 
-      expect(client).to have_received(:message_channel).with(
-        hash_including(text: /- #{job.title} at #{job.company}/),
+      expect(client).to have_received(:message).with(
+        a_string_including("**#{job.title}** at _#{job.company}_")
       )
     end
   end
@@ -26,17 +34,16 @@ RSpec.describe 'job_board:digest' do
     before { travel_to(Time.now.last_week(:tuesday)) }
     after { travel_back }
 
-    it 'does not post job listings to Slack' do
-      client = stub_slack_client
+    it 'does not post job listings to Discord' do
+      client = stub_discord_client
       Rake::Task['job_board:digest'].invoke
 
-      expect(client).not_to have_received(:message_channel)
+      expect(client).not_to have_received(:message)
     end
   end
 
-  def stub_slack_client
-    client = instance_double(SlackClient, message_channel: nil)
-    allow(SlackClient).to receive(:new).and_return(client)
+  def stub_discord_client
+    client = class_double(DiscordClient, message: nil).as_stubbed_const
     client
   end
 end
