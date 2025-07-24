44470eded572b700d2fd665f90ec2caed4593088
fatmahussein <fartunhussein346@gmail.com>
2025-07-24 10:39:37 +0300

    Update text content from slack to discord

--- app/javascript/components/home/ExceedYourProfessionalGoalsSection.jsx
+++ app/javascript/components/home/ExceedYourProfessionalGoalsSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ExceedYourProfessionalGoalsSection = () => {
     return (
         <p>
-            Use our #jobs channel on Slack to find your next opportunity. Join our CFP working group
+            Use our #jobs channel on Discord to find your next opportunity. Join our CFP working group
             to have experienced speakers review your conference proposal. Learn new technical skills
             at our meetup, and level up in your career. With support from the WNB.rb community, you
             can set more ambitious goals than ever before!
