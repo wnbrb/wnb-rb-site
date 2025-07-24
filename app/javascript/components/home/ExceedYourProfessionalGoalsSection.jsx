96c449c0e66b01144e00b5ad3625fae99fa98e55
fatmahussein <fartunhussein346@gmail.com>
2025-07-24 12:56:29 +0300

    fix prettier offense

--- app/javascript/components/home/ExceedYourProfessionalGoalsSection.jsx
+++ app/javascript/components/home/ExceedYourProfessionalGoalsSection.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const ExceedYourProfessionalGoalsSection = () => {
-    return (
-        <p>
-            Use our #jobs channel on Discord to find your next opportunity. Join our CFP working group
-            to have experienced speakers review your conference proposal. Learn new technical skills
-            at our meetup, and level up in your career. With support from the WNB.rb community, you
-            can set more ambitious goals than ever before!
-        </p>
-    );
+  return (
+    <p>
+      Use our #jobs channel on Discord to find your next opportunity. Join our
+      CFP working group to have experienced speakers review your conference
+      proposal. Learn new technical skills at our meetup, and level up in your
+      career. With support from the WNB.rb community, you can set more ambitious
+      goals than ever before!
+    </p>
+  );
 };
+
 export default ExceedYourProfessionalGoalsSection;
