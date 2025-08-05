688e0d1a5183cff01564c4c08e32a2d9614d35eb
Fatuma Hussein <fartunhussein346@gmail.com>
2025-08-05 12:40:10 +0300

    Rename slack to discord (#327)
    * Remove Slack-related variables
    
    * Remove slack-related gems
    
    * Update text content from slack to discord
    
    * migrate weekly job digest from Slack to Discord
    
    * Delete slack unused configuration files
    
    * Add WebMock and ClimateControl gems to support HTTP stubbing and ENV management in tests
    
    * Migrate job_board:digest task tests from Slack to Discord webhook
    
    * fix rubocop offense
    
    * allow localhost connections
    
    * fix prettier offense
    
    * fix prettier offense
    
    * Remove unused gems
    
    * Remove unused variable
    
    * Delete unused Job board files
    
    * remove WebMock setup in spec_helper now that rake task is removed
    
    * Revert "Remove unused gems"
    
    This reverts commit 3f7468d7456be4d40db8245fc8707041961a1068.
    
    * Remove unused gems

--- config/initializers/slack.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Slack.configure do |config|
-  config.token = ENV.fetch('SLACK_OAUTH_TOKEN', nil)
-end
-
-Slack::Events.configure do |config|
-  config.signing_secret = ENV.fetch('SLACK_SIGNING_SECRET', nil)
-end
