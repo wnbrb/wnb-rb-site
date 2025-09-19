dc13f2398d352ae04f9647ad69fa62bbcb9ff972
fatmahussein <fartunhussein346@gmail.com>
2025-09-19 10:13:25 +0300

    Migration to clean up existing speaker image URLs by removing leading/trailing whitespace

---
+++ db/migrate/20250919070027_clean_up_speaker_image_urls.rb
@@ -0,0 +1,10 @@
+class CleanUpSpeakerImageUrls < ActiveRecord::Migration[7.0]
+   def up
+    execute <<~SQL
+      UPDATE speakers
+      SET image_url = TRIM(image_url)
+      WHERE image_url IS NOT NULL;
+    SQL
+  end
+
+end
