c3c457e9da01003a48065685911c3b30f3f424e2
mayra lucia navarro <luciagirasoles@gmail.com>
2025-09-29 18:58:06 -0500

    Fix rubocop offense

--- config/boot.rb
+++ config/boot.rb
@@ -3,5 +3,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require "logger"        # Ensures that ::Logger exists before ActiveSupport
+require 'logger'        # Ensures that ::Logger exists before ActiveSupport
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
