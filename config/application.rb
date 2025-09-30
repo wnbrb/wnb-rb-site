6549dd3913233f1c23a92a15f937343280669aa9
mayra lucia navarro <luciagirasoles@gmail.com>
2025-09-29 20:58:10 -0500

    Update setup

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module WnbRbSite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -23,7 +23,6 @@ module WnbRbSite
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     config.time_zone = 'Eastern Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join("extras")
   end
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+# TODO: Restore deprecations warning after upgrading to Rails 7.2
+# Temporarily silence the deprecation warnings
+Rails.application.deprecators.silence do
+  # Initialize the Rails application.
   Rails.application.initialize!
+end
