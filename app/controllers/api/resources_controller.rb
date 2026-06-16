68d2ac072e51c6a36f7a007e96b1c399c34f2798
fatmahussein <fartunhussein346@gmail.com>
2026-06-16 17:11:06 +0300

    add public API for listing and creating resources

---
+++ app/controllers/api/resources_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Api
+  class ResourcesController < ApplicationController
+    protect_from_forgery with: :null_session
+
+    def index
+      resources = Resource.recent
+      resources = resources.by_category(params[:category]) if params[:category].present?
+      render json: { data: resources.map(&:as_json) }
+    end
+
+    def create
+      resource = Resource.new(resource_params)
+
+      if resource.save
+        render json: { message: "Resource submitted successfully!" }, status: :created
+      else
+        render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def resource_params
+      params.permit(:title, :url, :description, :category, :submitted_by)
+    end
+  end
+end
