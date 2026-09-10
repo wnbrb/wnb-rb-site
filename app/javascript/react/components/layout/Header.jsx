abee52246b52c2677b82ee603cebb51c8cfd56b8
fatmahussein <fartunhussein346@gmail.com>
2026-09-10 14:06:03 +0300

    Resolve merge conflict in Header, add Resources nav link

--- app/javascript/react/components/layout/Header.jsx
+++ app/javascript/react/components/layout/Header.jsx
@@ -7,16 +7,11 @@ const Header = () => {
     const links = [
         { id: 1, text: 'Events', href: '/meetups' },
         { id: 2, text: 'Our Community', href: '/community' },
-<<<<<<< HEAD
         { id: 3, text: 'Resources', href: '/resources' },
-        { id: 4, text: 'Donate', href: 'https://buy.stripe.com/6oE7t874ReRc7gA9AN' },
-        { id: 5, text: 'Support us', href: '/sponsor-us' },
+        { id: 4, text: 'Donate', href: 'https://givebutter.com/wnbrb-our-next-chapter' },
+        { id: 5, text: 'Sponsor us', href: '/sponsor-us' },
         { id: 6, text: 'Join', href: '/join-us' },
-=======
-        { id: 3, text: 'Donate', href: 'https://givebutter.com/wnbrb-our-next-chapter' },
-        { id: 4, text: 'Sponsor us', href: '/sponsor-us' },
-        { id: 5, text: 'Join', href: '/join-us' },
->>>>>>> origin/main
+
     ];
 
     const [headerState, setHeaderState] = useState({
